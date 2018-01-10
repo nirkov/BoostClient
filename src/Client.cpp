e5cc2c07def3967b9ff4ea4efaa6264ad62a46ab
nirkov <nir.kovrovski@gmail.com>
2018-01-10 20:21:46 +0200

    10.1.2018

--- src/Client.cpp
+++ src/Client.cpp
@@ -2,9 +2,52 @@
 // Created by nir on 10/01/18.
 //
 
-#include <stdlib.h>
+
 #include "../include/connectionHandler.h"
+#include <boost/thread.hpp>
 using namespace std;
+
+
+class GetFromKeyboard {
+private:
+    ConnectionHandler* CONNECTION_HANDLER = nullptr;
+
+public:
+    GetFromKeyboard(ConnectionHandler* newConHan) : CONNECTION_HANDLER(newConHan){}
+
+    void operator()(){
+        while (1) {
+            const short bufsize = 1024;
+            char buf[bufsize];
+            std::cin.getline(buf, bufsize);
+            std::string line(buf);
+            int len = line.length();
+            if (!(*CONNECTION_HANDLER).sendLine(line)) {
+                std::cout << "Disconnected. Exiting...\n" << std::endl;
+                break;
+            }
+        }
+    };
+};
+
+
+class GetFromServer {
+private:
+    ConnectionHandler* CONNECTION_HANDLER_SERVER = nullptr;
+
+public:
+    GetFromServer(ConnectionHandler* newConHan) : CONNECTION_HANDLER_SERVER(newConHan){}
+
+    void operator()(){
+        while (1) {
+            std::string answer;
+            if (!CONNECTION_HANDLER_SERVER->getLine(answer)) {
+                std::cout << "Disconnected. Exiting...\n" << std::endl;
+                break;
+            }
+        }
+    };
+};
 
 int main (int argc, char *argv[]) {
 
@@ -15,6 +58,20 @@ int main (int argc, char *argv[]) {
     short port = 7777;
 
     ConnectionHandler connectionHandler(host, port);
+    if (!connectionHandler.connect()) {
+        std::cerr << "Cannot connect to " << host << ":" << port << std::endl;
+        return 1;
+    }
 
+    GetFromKeyboard fromKeyboard(&connectionHandler);
+    GetFromServer fromServer(&connectionHandler);
+
+
+    boost::thread getFromKeyBoard_Theard(fromKeyboard);
+    boost::thread getFromServer_Theard(fromServer);
+
+    getFromKeyBoard_Theard.join();
+    getFromServer_Theard.join();
 
 }
+
